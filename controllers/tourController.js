eaf04fa3eb259e3a930ec4ce240fe7b6c34988ab
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-05-17 11:38:11 +0800

    feat: Add tour create function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('../models/tourModel')
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    })
-  }
-  next()
-}
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -26,20 +16,21 @@ exports.getTourById = (req, res) => {
   // })
 }
 
-exports.createTour = (req, res) => {
-  // const newId = tours[tours.length - 1].id + 1
-  // const newTour = Object.assign({ id: newId }, req.body)
-  // tours.push(newTour)
-  // fs.writeFile(
-  //   `${__dirname}/dev-data/data/tours-simple.json`,
-  //   JSON.stringify(tours),
-  //   (err) => {
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: { tour: newTour },
-  //     })
-  //   }
-  // )
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = await Tour.create(req.body)
+    // newTour.save()
+    const newTour = await Tour.create(req.body)
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour },
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    })
+  }
 }
 
 exports.updateTour = (req, res) => {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,13 +5,12 @@ const {
   createTour,
   updateTour,
   deleteTour,
-  checkBody,
 } = require('./../controllers/tourController')
 const router = express.Router()
 
 // router.param('id', checkID)
 
-router.route('/').get(getAllTours).post(checkBody, createTour)
+router.route('/').get(getAllTours).post(createTour)
 router.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour)
 
 module.exports = router
