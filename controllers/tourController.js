b9dc9797703f6256085316f1108c0257c78b0717
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-05-24 00:42:07 +0800

    feat: Add sorting feature

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,7 +41,15 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(parsedQueryObj)
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
 
-    const query = Tour.find(JSON.parse(queryStr))
+    let query = Tour.find(JSON.parse(queryStr))
+
+    // 4) sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ')
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort('-createdAt')
+    }
 
     // EXECUTE QUERY
     const tours = await query
