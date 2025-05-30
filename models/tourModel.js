3226457c65ac985aa6c8989118717661395eb9a4
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-05-30 22:26:39 +0800

    feat: Add query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -57,6 +57,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -71,6 +75,29 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() and .insertMany()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true })
+  next()
+})
+
+// tourSchema.pre('save', function (next) {
+//   console.log('will save document...')
+//   next()
+// })
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc)
+//   next()
+// })
+
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } })
+
+  this.start = Date.now()
+  next()
+})
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`)
   next()
 })
 
