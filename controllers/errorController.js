ffd5e78c65b48ab97af43c8c85e023690375451f
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-06-02 23:46:55 +0800

    feat: Handling Mongoose Validation Errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -11,6 +11,12 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400)
 }
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message)
+  const message = `Invalid input data. ${errors.join('. ')}`
+  return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -47,7 +53,6 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res)
   } else {
-    console.log('NODE_ENV not set or unknown, defaulting to production mode')
     let error = { ...err }
 
     if (err.name === 'CastError' || err.constructor.name === 'CastError') {
@@ -56,6 +61,13 @@ module.exports = (err, req, res, next) => {
 
     if (err.code === 11000) {
       error = handleDuplicateFieldsDB(err)
+    }
+
+    if (
+      err.name === 'ValidationError' ||
+      err.constructor.name === 'ValidationError'
+    ) {
+      error = handleValidationErrorDB(err)
     }
 
     sendErrorProd(error, res)
