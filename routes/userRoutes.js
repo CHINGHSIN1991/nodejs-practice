af227d7175d697f2278c56b4e0470861d9adb242
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-06-06 00:19:54 +0800

    feat: Add login feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,7 @@ const {
 const router = express.Router()
 
 router.post('/signup', authController.signup)
+router.post('/login', authController.login)
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser)
