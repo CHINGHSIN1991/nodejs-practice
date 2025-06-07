faf65e041231d22ce2e400a15a30006e89cc96e4
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-06-07 23:17:54 +0800

    fix: Fix login authentication and error handling logic; improve error handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,4 @@
-const AppError = require('../utils/appError')
+const AppError = require('./../utils/appError')
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`
@@ -16,6 +16,12 @@ const handleValidationErrorDB = (err) => {
   const message = `Invalid input data. ${errors.join('. ')}`
   return new AppError(message, 400)
 }
+
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401)
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401)
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -52,23 +58,14 @@ module.exports = (err, req, res, next) => {
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res)
-  } else {
-    let error = { ...err }
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = err
 
-    if (err.name === 'CastError' || err.constructor.name === 'CastError') {
-      error = handleCastErrorDB(err)
-    }
-
-    if (err.code === 11000) {
-      error = handleDuplicateFieldsDB(err)
-    }
-
-    if (
-      err.name === 'ValidationError' ||
-      err.constructor.name === 'ValidationError'
-    ) {
-      error = handleValidationErrorDB(err)
-    }
+    if (error.name === 'CastError') error = handleCastErrorDB(error)
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error)
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error)
+    if (error.name === 'JsonWebTokenError') error = handleJWTError()
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError()
 
     sendErrorProd(error, res)
   }
