d9e52ba9489096d0a48d66cee0251c79ed2b4027
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-06-10 23:42:26 +0800

    feat: Add forget password feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,8 @@ const router = express.Router()
 
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
+router.post('/forgotPassword', authController.forgotPassword)
+router.patch('/resetPassword/:token', authController.resetPassword)
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser)
