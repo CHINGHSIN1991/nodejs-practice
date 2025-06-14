892833ed8128d0335772361716c478f2c66ff501
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-06-14 19:53:23 +0800

    feat: Add user info update feature to support name and email changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,29 @@
 const express = require('express')
-const authController = require('./../controllers/authController')
+const {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  protect,
+} = require('./../controllers/authController')
 const {
   getAllUsers,
   createUser,
   getUserById,
   updateUser,
   deleteUser,
+  updateMe,
 } = require('./../controllers/userController')
 
 const router = express.Router()
 
-router.post('/signup', authController.signup)
-router.post('/login', authController.login)
-router.post('/forgotPassword', authController.forgotPassword)
-router.patch('/resetPassword/:token', authController.resetPassword)
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-)
+router.post('/signup', signup)
+router.post('/login', login)
+router.post('/forgotPassword', forgotPassword)
+router.patch('/resetPassword/:token', resetPassword)
+router.patch('/updateMyPassword', protect, updatePassword)
+router.patch('/updateMe', protect, updateMe)
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser)
