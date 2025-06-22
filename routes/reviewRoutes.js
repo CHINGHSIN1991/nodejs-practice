02c9283e8843b4759cf5173dc2a52b48d1a8e5dd
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-06-22 16:05:56 +0800

    feat: Add review feature

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const {
+  getAllReviews,
+  createReview,
+} = require('./../controllers/reviewController')
+const authController = require('./../controllers/authController')
+const router = express.Router({ mergeParams: true })
+
+router
+  .route('/')
+  .get(getAllReviews)
+  .post(authController.protect, authController.restrictTo('user'), createReview)
+
+module.exports = router
