922ddb46b07d8e20485767e735ac773b3af2cabc
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-06-26 00:35:41 +0800

    refactor: use factory pattern for controllers to simplify code, improve reusability, and add support for fetching single or multiple documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,6 @@
 const Tour = require('../models/tourModel')
-const APIFeatures = require('../utils/apiFeatures')
 const catchAsync = require('../utils/catchAsync')
-const AppError = require('../utils/appError')
+// const AppError = require('../utils/appError')
 const factory = require('./handlerFactory')
 
 exports.aliasTopTours = (req, res, next) => {
@@ -11,37 +10,10 @@ exports.aliasTopTours = (req, res, next) => {
   next()
 }
 
-exports.getAllTours = catchAsync(async (req, res, next) => {
-  // EXECUTE QUERY
-  const features = new APIFeatures(Tour.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate()
-  const tours = await features.query
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: { tours },
-  })
+exports.getAllTours = factory.getAll(Tour)
+exports.getTour = factory.getOne(Tour, {
+  path: 'reviews',
 })
-
-exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id).populate({
-    path: 'reviews',
-    select: 'review rating user -tour',
-  })
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404))
-  }
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-  })
-})
-
 exports.createTour = factory.createOne(Tour)
 exports.updateTour = factory.updateOne(Tour)
 exports.deleteTour = factory.deleteOne(Tour)
