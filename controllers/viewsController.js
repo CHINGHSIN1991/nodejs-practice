6c51064adb10146a793f01f818d136d091baebd5
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-07-13 23:40:03 +0800

    refactor: Modify file structure

---
+++ controllers/viewsController.js
@@ -0,0 +1,15 @@
+const Tour = require('../models/tourModel')
+
+exports.getOverview = async (req, res) => {
+  const tours = await Tour.find()
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  })
+}
+
+exports.getTour = (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  })
+}
