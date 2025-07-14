2e3eb8c11aef803c7e1949820db360c398e00b33
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-07-14 23:04:22 +0800

    feat: Building the tour overview

--- app.js
+++ app.js
@@ -74,30 +74,6 @@ app.use((req, res, next) => {
 })
 
 // Mounting the routes
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-  })
-})
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  })
-})
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  })
-})
-
-app.get('/login', (req, res) => {
-  res.status(200).render('login', {
-    title: 'Log into your account',
-  })
-})
-
 app.use('/', viewRouter)
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
